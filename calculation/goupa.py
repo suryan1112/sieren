6e77b05f2ca8a01a8da23479a52ab7d3ec8b9f0b
suryan1112 <139356206+suryan1112@users.noreply.github.com>
2024-04-10 16:12:34 +0530

    first beta version

--- calculation/goupa.py
+++ calculation/goupa.py
@@ -15,16 +15,19 @@ def get_sgpa(roll_arr):
     
     same_branch=np.all([roll[2]==roll_arr[0][2] for roll in roll_arr])
     same_semester=np.all([roll[3]==roll_arr[0][3] for roll in roll_arr])
+    
+    substring_set = {element[2:4] for element in roll_arr}
+    credits=creditz_creater(substring_set)
 
     for roll in roll_arr:
         print(roll)
         
-        touple=fetching(roll) # main step...
+        touple=fetching(roll,credits) # main step...
         if touple is None: continue
-        (enrollment_number,roll_number,student_name,sgpa,marks,subject_arr)=touple 
+        (enrollment_number,roll_number,student_name,sgpa,marks,subject_arr,Total_marks)=touple 
         
         class_room=f'{(branch_names[roll[2]][-1]).upper()}{"" if roll[2] in non_sectional_branch else "."+chr(int(roll[4])+65)}'
-        list=[enrollment_number, roll_number,class_room, student_name, sgpa,backs(marks)]
+        list=[enrollment_number, roll_number,class_room, student_name, sgpa,Total_marks,backs(marks)]
         
         if same_branch: list.pop(2)
     
@@ -46,14 +49,14 @@ def get_sgpa(roll_arr):
        
     if same_branch and same_semester:
         print('branches and semester are all same 👌')
-        frame_columns = [("Enrollment", '', ''), ("Roll.No", '', ''), ("_Name_of_Student___", '', ''), ("SGPA", '', ''), ("Back_logs", '', ''),*subject_arr[0]]
+        frame_columns = [("Enrollment", '', ''), ("Roll.No", '', ''), ("_Name_of_Student___", '', ''), ("SGPA", '', ''),("Marks",'',''), ("Back_logs", '', ''),*subject_arr[0]]
         
     elif same_branch:
         print('branches are all same 👌')
-        frame_columns=[ ("Enrollment",''), ("Roll.No",''), ("_Name_of_Student___",''), ("SGPA",''),("Back_logs",''),*sub1]
+        frame_columns=[ ("Enrollment",''), ("Roll.No",''), ("_Name_of_Student___",''), ("SGPA",''),("Marks",''),("Back_logs",''),*sub1]
     else: 
         print('branches are not same 🙌')        
-        frame_columns=[ ("Enrollment",''), ("Roll.No",''),("branch",''), ("_Name_of_Student___",''), ("SGPA",''),("Back_logs",''),*sub1]
+        frame_columns=[ ("Enrollment",''), ("Roll.No",''),("branch",''), ("_Name_of_Student___",''), ("SGPA",''),("Marks",''),("Back_logs",''),*sub1]
    
     frame=pd.DataFrame(data_list,columns=pd.MultiIndex.from_tuples(frame_columns))
     frame.dropna(axis=1, how='all',inplace=True)
--- calculation/helper.py
+++ calculation/helper.py
@@ -1,3 +1,6 @@
+from sieren.source.soma import *
+from sieren.calculation.creditz import *
+from sieren.source.testa import *
 
 def backs(marks):
     fail_count = 0
@@ -12,14 +15,30 @@ def backs2(subjects_details):
             subject_data=j.find_all('td')
             marks.append(subject_data[-2].string)
         return marks.count('F')+marks.count('Ab')      
-       
-#link = https://www.ietdavv.edu.in/index.php/be-iiiyr-civ-sem-a       #for creadits fectching
+        
+def creditz_creater(substring_set):
+    credits={}
+    print(substring_set)
+    for i in substring_set:
+        branch=i[0]
+        if branch not in credits:
+            credits[branch]={}    
+        credits[branch].update(credit2_subject(f'21{i}023'))
+    return credits
 
-def subject_handeler(subjects_details):
+    
+def subject_handeler(subjects_details,credits,branch):
+    # print(credits)
     subjects,marks=[],[]
     creditz2=None
+    Total_marks=0
+    
     for j in subjects_details[1:]:
         subject_data=j.find_all('td')
+        
+        if move_number_to_beginning(subject_data[1].string) in credits[branch]:
+            credit=credits[branch][move_number_to_beginning(subject_data[1].string)]
+        else: credit=(0,7)    
         
         if 'SVR' in subject_data[1].string :
             creditz2=subject_data
@@ -28,16 +47,31 @@ def subject_handeler(subjects_details):
         subjects.append((subject_data[0].string,subject_data[1].string,'Theory'))
         subjects.append((subject_data[0].string,subject_data[1].string,'Practical'))
         
-        marks.append(subject_data[-2].string)
-        marks.append(subject_data[-1].string)
+        #THEORY...
+        grade=grd[subject_data[-2].string]
+        marks.append(superscript[grade] +'*'+superscript[credit[0]]+'  '+subject_data[-2].string)
+        Total_marks+=grade*credit[0]
+        
+        #PRACTICAL...
+        grade=grd[subject_data[-1].string]
+        marks.append(superscript[grade] +'*'+superscript[credit[1]]+'  '+subject_data[-1].string)
+        Total_marks+=grade*credit[1]
     
     if(creditz2):
         subject_data=creditz2
+        credit=credits[branch][move_number_to_beginning(subject_data[1].string)][:]
         
         subjects.append((subject_data[0].string,subject_data[1].string,'Theory'))
         subjects.append((subject_data[0].string,subject_data[1].string,'Practical'))
         
-        marks.append(subject_data[-2].string)
-        marks.append(subject_data[-1].string)
+        #THEORY...
+        grade=grd[subject_data[-2].string]
+        marks.append(superscript[grade] +'*'+superscript[credit[0]]+'  '+subject_data[-2].string)
+        Total_marks+=grade*credit[0]
         
-    return (subjects,marks)
+        #PRACTICAL...
+        grade=grd[subject_data[-1].string]
+        marks.append(superscript[grade] +'*'+superscript[credit[1]]+'  '+subject_data[-1].string)
+        Total_marks+=grade*credit[1]
+        
+    return (subjects,marks,Total_marks)
