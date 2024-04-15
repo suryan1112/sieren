da08e450fceba90b25b09eb76bb69e61f58235ed
suryan gupta <139356206+suryan1112@users.noreply.github.com>
2024-04-15 23:41:29 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,3 @@ from sieren.source.testa import *
 from sieren.calculation.goupa import get_cgpa,get_sgpa
 from branchiole import branchwise_sgpa,branchwise_cgpa,complete_branch
 from yeariole import yearly_cgpa,yearly_sgpa
-
-df=get_sgpa(['21I5054'])
-print(df)
