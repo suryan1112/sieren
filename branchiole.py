f1f9e1e47bf08e5067c5d25ec65d56814d121978
suryan1112 <139356206+suryan1112@users.noreply.github.com>
2024-04-25 23:53:01 +0530

    alkdsjf

--- branchiole.py
+++ branchiole.py
@@ -1,15 +1,17 @@
 from sieren.calculation.goupa import get_cgpa,get_sgpa
 from sieren.source.helper import *
+from sieren.export.runner import *
 
-def branchwise_sgpa(addmission_year,branch,semester,section=None,sorted=True):
+def branchwise_sgpa(addmission_year,branch,semester,section=None,sorted=True,force_full=False):
     
     branch= find_parent_branch(branch)
     if section: section=section.upper()
     if branch is None: 
-        print('Branch is not correct ❌')
+        print('Incorrect Branch ❌')
         return     
     file_name=file_name_creater(semester,addmission_year,sorted,branch,section,category='SGPA+MARKS')
-      
+    
+    if alternative_flow(file_name) and not force_full: return
     roll_arr=get_roll_numbers(addmission_year,semester,branch,section)
             
     frame=get_sgpa(roll_arr)  # main step....
@@ -20,15 +22,16 @@ def branchwise_sgpa(addmission_year,branch,semester,section=None,sorted=True):
     save_file(frame,file_name)
     return frame
 
-def branchwise_cgpa(addmission_year,branch,semester,section=None,sorted=True):
+def branchwise_cgpa(addmission_year,branch,semester,section=None,sorted=True,force_full=False):
     
     branch= find_parent_branch(branch)
     if section: section=section.upper()
     if branch is None: 
-        print('Branch is not correct ❌')
+        print('Incorrect Branch ❌')
         return     
     file_name=file_name_creater(semester,addmission_year,sorted,branch,section,category='CGPA')
-      
+    
+    if alternative_flow(file_name) and not force_full: return  
     roll_arr=get_roll_numbers(addmission_year,semester,branch,section)
             
     frame=get_cgpa(roll_arr)  # main step....
@@ -41,15 +44,16 @@ def branchwise_cgpa(addmission_year,branch,semester,section=None,sorted=True):
 
 
 
-def complete_branch(last_year,branch,last_year_semester,section=None,sorted=True):
+def complete_branch(last_year,branch,last_year_semester,section=None,sorted=True,force_full=False):
     
     branch= find_parent_branch(branch)
     if section: section=section.upper()
     if branch is None: 
-        print('Branch is not correct ❌')
+        print('Incorrect Branch ❌')
         return     
     file_name=create_file_name(branch,sorted,section,category='SGPA+MARKS')
     
+    if alternative_flow(file_name) and not force_full: return
     roll_arr=[]
     for i in range(0,4): 
         roll_arr.extend(get_roll_numbers(last_year,last_year_semester,branch,section))
